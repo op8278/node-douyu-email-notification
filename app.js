9c19f76186db82d251f18cd1cde3cb2cab354d8f
op8278 <406386343@qq.com>
2018-01-11 18:40:23 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,51 @@
+const nodemailer = require('nodemailer')
+
+const { sleep, getAllDouyuRoomInfoPromise, sendMail } = require('./util')
+const config = require('./config.js')
+
+let preStreamState = {} // 主播之前的开播状态
+
+nodemailer.createTestAccount((err, account) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport(config.transporter)
+  // 开启循环监听
+  monitor(transporter)
+})
+
+// 监听房间状态
+async function monitor(transporter) {
+  try {
+    const resultArray = await getAllDouyuRoomInfoPromise()
+    for (let value of resultArray) {
+      const { data: { data: roomInfo, error: errorCode } } = value
+      // 判断状态
+      if (errorCode !== 0 ) {
+        throw new Error(roomInfo)
+      }
+      // 判断是否开播
+      // "1" - 开播 , "2" - 未开播
+      if (roomInfo.room_status === '1') {
+        console.log(`${roomInfo.owner_name} ---- 已经开播 ---- ${roomInfo.start_time}`)
+        if (!preStreamState[roomInfo.room_id]) {
+          // 配置发送邮件信息
+          const mailOptions = Object.assign(config.emailDetail, { 
+            subject: roomInfo.owner_name, // 邮箱标题
+            text: roomInfo.room_name + roomInfo.start_time // 邮箱内容
+          })
+          // 发送邮件
+          sendMail(transporter, mailOptions)
+          preStreamState[roomInfo.room_id] = true
+        }
+      } else {
+        console.log(`${roomInfo.owner_name} ---- 未开播 ---- 上次开播时间 ---- ${roomInfo.start_time}`)
+        preStreamState[roomInfo.room_id] = false
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  } finally {
+    // 隔一段时间再请求
+    await sleep()
+    return monitor(transporter)
+  }
+}
---
+++ util.js
@@ -0,0 +1,58 @@
+const axios = require('axios')
+const config = require('./config.js')
+
+// 延迟函数
+function sleep(ms = 1000 * 10) { // 默认10秒
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+// 根据斗鱼房间号,判断是否在开播状态
+function isOnStream(roomId) {
+  return axios.get(config.douyu.roomApi + roomId).then((res) => {
+    const responseData = res.data
+    if (responseData.error != '0') {
+      console.log('请求错误')
+      // TODO: 错误处理
+      return false
+    }
+    // 判断是否在开播状态
+    // 1 - 开播 , 2 - 未开播
+    if (responseData.data.room_status != 1) {
+      return false
+    }
+    return true
+  })
+}
+// 通过RoomId,获取斗鱼房间信息
+function getDouyuRoomInfoByRoomId(roomId) {
+  try {
+    return axios.get(config.douyu.roomApi + roomId)
+  } catch (error) {
+    throw error
+  }
+}
+// 获取RoomId列表上,所有斗鱼房间信息
+function getAllDouyuRoomInfoPromise(roomIdArray = config.douyu.roomId) {
+  let douyuRoomInfoPromiseArray = []
+  for (let roomId of roomIdArray) {
+    douyuRoomInfoPromiseArray.push(getDouyuRoomInfoByRoomId(roomId))
+  }
+  return Promise.all(douyuRoomInfoPromiseArray)
+}
+// 发送邮件
+function sendMail(transporter, mailOptions) {
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message sent: %s', info.messageId)
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+  })
+}
+module.exports = {
+  sleep,
+  isOnStream,
+  getDouyuRoomInfoByRoomId,
+  getAllDouyuRoomInfoPromise,
+  sendMail,
+}
