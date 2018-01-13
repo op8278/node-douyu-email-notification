5f8d5a29836bff7e503eeef9ce18a34563449ced
op8278 <406386343@qq.com>
2018-01-13 21:13:18 +0800

    modify README again

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const senderEMail = '请填写发送方的邮箱帐号' // 你的邮箱帐号(发送方)
+const senderEmail = '请填写发送方的邮箱帐号' // 你的邮箱帐号(发送方)
 const receiverEmail = '请填写接收方的邮箱帐号' // 接收方邮箱帐号(接收方)
 
 module.exports = {
@@ -9,7 +9,7 @@ module.exports = {
     port: 465, // 端口
     secure: true, // true for 465, false for other ports
     auth: {
-      user: senderEMail, // 你的邮箱帐号
+      user: senderEmail, // 你的邮箱帐号
       pass: '请填写你的邮箱授权码', // 你的邮箱SMTP密码(授权码)
     }
   },
@@ -17,13 +17,13 @@ module.exports = {
   douyu: {
     roomApi: `http://open.douyucdn.cn/api/RoomApi/room/`,
     roomId: [ // 斗鱼房间号或者别名
-      'zard'
-      // '58428' // yyf房间
+      'zard',
+      '58428' // yyf房间
     ],
   },
   // TODO: 改为模版
   emailDetail: {
-    from: `"斗鱼开播提醒" <${senderEMail}>`, // sender addressss
+    from: `"斗鱼开播提醒" <${senderEmail}>`, // sender addressss
     to: receiverEmail, // list of receivers
     subject: 'Zard开播了', // Subject line
     text: '没有内容', // plain text body
